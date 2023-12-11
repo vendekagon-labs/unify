973454d1c9affd2564f45b537776f105ba9495df
Ben Kamphaus <ben.kamphaus@gmail.com>
2023-12-10 17:18:06 -0700

    clean up unify executable

--- src/com/vendekagonlabs/unify/cli.clj
+++ src/com/vendekagonlabs/unify/cli.clj
@@ -29,6 +29,17 @@
   (:gen-class)
   (:import (java.util Date)))
 
+
+(def unify-ascii "
+==================================================
+   __    __  .__   __.  __   ___________    ____
+  |  |  |  | |  \\ |  | |  | |   ____\\   \\  /   /
+  |  |  |  | |   \\|  | |  | |  |__   \\   \\/   /
+  |  |  |  | |  . `  | |  | |   __|   \\_    _/
+  |  `--'  | |  |\\   | |  | |  |        |  |
+   \\______/  |__| \\__| |__| |__|        |__|
+==================================================")
+
 ;; this handles case when pmap, etc. cause exceptions outside main
 ;; thread that don't hit normal exception handling.
 ;; see: https://stuartsierra.com/2015/05/27/clojure-uncaught-exceptions
@@ -38,9 +49,6 @@
       (log/error ex "Uncaught exception on" (.getName thread))
       (cli.error-handling/report-and-exit ex))))
 
-
-;; Usage --------------------------------------------------------------------
-;;
 
 (defn usage [options-summary]
   (->> [""
@@ -56,58 +64,55 @@
         "  delete-db         Deletes the database specified by --database"
         "  prepare           Uses an import config file to generate all data needed to run an import."
         "                    Requires --import-config, --working-directory and --schema-directory args."
-        "  diff              Generates all changes required to update an existing dataset to match the target."
-        "                    Requires --working-directory and --database arguments."
+;;        "  diff              Generates all changes required to update an existing dataset to match the target."
+;;        "                    Requires --working-directory and --database arguments."
         "  transact          Transacts all data (as created by prepare) for an import job --working-directory"
         "                    into database specified by --database."
         "                    Optionally use --update to transact updated changes instead of prepared transactions."
         "                    If an imports fails due to eg network latency or process termination, you can resume"
-        "                    with --resume"
+        "                    with --resume" ""]
 ;;        "  validate          Runs validation checks against the database."
 ;;        "                    Requires --working-directory and --database arguments when working with branch databases."
-        "  crosscheck-reference   Checks all reference data files for potential upsert collisions."
-        "                         Requires --database and --working-directory args."
-        ""]
+;;         "  crosscheck-reference   Checks all reference data files for potential upsert collisions."
+;;        "                         Requires --database and --working-directory args."
        (str/join \newline)))
 
 (def cli-options
-  [[nil "--import-config     IMPORT-CONFIG" "Import Config edn file (required for prepare)"]
-   [nil "--working-directory IMPORT-WORKING-DIRECTORY"
+  [[nil "--import-config     IMPORT-CONFIG" "Import config edn file"]
+   [nil "--working-directory WORKING-DIRECTORY"
         "Directory where prepared data goes, transact uses data prepare puts here."]
-   [nil "--schema-directory IMPORT-SCHEMA-DIRECTORY"]
-   [nil "--tx-batch-size     TX-BLOCK-SIZE" "Datomic transaction batch size"
+   [nil "--schema-directory  SCHEMA-DIRECTORY"
+    "Directory containing a Unify schema (base Datomic schema + metamodel annotations)"]
+   [nil "--tx-batch-size     TX-BATCH-SIZE" "Datomic transaction batch size"
     :default 50
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 20 % 200) "Transaction batch size must be between 20 and 200 in length."]]
-   [nil "--validations       VALIDATIONS-EDN" (str "An edn file containing maps with name, query, and expected keys which "
-                                                   "defines a set of queries to run on db in order to detect invalid db states.")]
    [nil "--database          DATABASE-NAME" "The name of the database to perform command against."]
    ;; --overwrite option disabled for now as it exhibits inconsistent behavior on different OSes, directory structures.
    ; [nil "--overwrite" "When set on prepare, will delete and rewrite working directory if it already exists."]
-   [nil "--resume" "When set, will resume a previously started transact command."]
+   [nil "--resume" "When set, will resume a previously started transact command."
+    :default false]
    [nil "--update" "When set, will transact the changes generated from diff instead of prepare"
     :default false]
-   [nil "--skip-annotations" "When set, will not transact annotations into database."]
-   [nil "--continue-on-error" "When set, unify prepare will continue when encountering errors and report all errors at end."]
+   [nil "--skip-annotations" "When set, will not transact annotations into database."
+    :default false]
+   [nil "--continue-on-error" "When set, unify prepare will continue when encountering errors and report all errors at end."
+    :default false]
    ["-h" "--help"]])
 
 
 (defn print-db-version [datomic-uri]
   (println "Target db schema version" (db/version datomic-uri)))
 
-;; Tasks ---------------------------------------------------------------
-;;
-
 (defn request-db
   [{:keys [database
            schema-directory] :as args}]
-  (if database
+  (if-not database
+    (exit 1 (str "ERROR: request-db requires a --database argument"))
     (try
       (let [result (backend/request-db database)]
         (if (:error result)
-          (do
-            (println "Error requesting database" database)
-            (pprint result))
+          (exit 1 (str "ERROR: request-db failed with: " result))
           (let [db-info (db/fetch-info database)
                 uri (:uri db-info)]
             (db/init uri :schema-directory schema-directory)
@@ -118,9 +123,7 @@
                      (when-let [err-data (ex-data e)]
                        (text/->pretty-string (dissoc err-data :message)))
                      \newline
-                     (.getMessage e)))))
-    (exit 1 (str "ERROR: requires --database argument"))))
-
+                     (.getMessage e)))))))
 
 (defn list-dbs [_]
   (let [result (backend/list-dbs)]
@@ -142,11 +145,11 @@
 
 (defn prepare
   [{:keys [import-cfg-file target-dir resume overwrite
-           schema-directory continue-on-error] :as ctx}]
+           schema-directory continue-on-error] :as arg-map}]
   (when-not schema-directory
     (exit 1 (str "ERROR: prepare requires --schema-directory argument.")))
   (when continue-on-error
-    (println "Set to attempt to continue when prepare encounters errors. Will report all errors at end and in logs."))
+    (println "Unify will attempt to continue when prepare encounters errors. Will report all errors at end and in logs."))
   (when-not (and import-cfg-file
                  (util.io/exists? import-cfg-file))
     (exit 1 (str "ERROR: Import config not specified or does not exist: " import-cfg-file)))
@@ -162,7 +165,7 @@
              (println "Working directory exists, *overwrite* is deleting:" target-dir)
              overwrite)
     (util.io/delete-recursively target-dir))
-  (import/prepare-import ctx))
+  (import/prepare-import arg-map))
 
 
 (defn transact
@@ -184,77 +187,74 @@
                " transactions, entire import job at " target-dir))))
 
 
-(defn diff
-  [{:keys [target-dir resume datomic-uri skip-annotations database] :as ctx}]
-  (when-not (and datomic-uri database)
-    (exit 1 "ERROR: Diff needs a database to transact to."))
-  (print-db-version datomic-uri)
-  (when resume
-    (println (str "WARN: Resuming transaction job. This will skip transacting the import job entity. "
-                  "Transactions may take awhile to restart as previously successful IDs are found.")))
-  (when-not (and target-dir (util.io/exists? target-dir))
-    (exit 1 (str "ERROR: Working Directory arg must be passed, directory must exist, and "
-                 "it must already contain transaction and entity data as created by prepare.")))
+#_(defn diff
+    [{:keys [target-dir resume datomic-uri skip-annotations database] :as ctx}]
+    (when-not (and datomic-uri database)
+      (exit 1 "ERROR: Diff needs a database to transact to."))
+    (print-db-version datomic-uri)
+    (when resume
+      (println (str "WARN: Resuming transaction job. This will skip transacting the import job entity. "
+                    "Transactions may take awhile to restart as previously successful IDs are found.")))
+    (when-not (and target-dir (util.io/exists? target-dir))
+      (exit 1 (str "ERROR: Working Directory arg must be passed, directory must exist, and "
+                   "it must already contain transaction and entity data as created by prepare.")))
 
-  (let [returned (import/perform-diff ctx)]
-    (if-let [errors (:errors returned)]
-      (do (println "Diff did not complete successfully, see logs. Anomalies: " errors)
-          returned)
-      (println "\nCompleted. Diff data in: "
-               (conventions/diff-tx-dir target-dir)))))
+    (let [returned (import/perform-diff ctx)]
+      (if-let [errors (:errors returned)]
+        (do (println "Diff did not complete successfully, see logs. Anomalies: " errors)
+            returned)
+        (println "\nCompleted. Diff data in: "
+                 (conventions/diff-tx-dir target-dir)))))
 
-(defn validate
-  [{:keys [working-directory database] :as ctx}]
-  (import/validate {:database database
-                    :working-directory working-directory}))
+#_(defn validate
+    [{:keys [working-directory database] :as ctx}]
+    (import/validate {:database database
+                      :working-directory working-directory}))
 
 
-(defn format-crosscheck-conflict
-  "Formatter to make crosscheck conflict results easier to read."
-  [{:keys [transaction-entity database-entity difference]}]
-  (let [{:keys [filename line-number]} (:unify/annotations transaction-entity)]
-    (str "File: " filename " at line-number: " line-number "\n"
-         (apply str
-                (text/->pretty-string (dissoc transaction-entity :unify/annotations :unify/identifying-avpair))
-                (for [attr (keys difference)]
-                  (str "\nTransaction file specified " attr " with value "
-                       (get transaction-entity attr)
-                       "\nDatabase contains " attr " with value "
-                       (get database-entity attr)))))))
+#_(defn format-crosscheck-conflict
+    "Formatter to make crosscheck conflict results easier to read."
+    [{:keys [transaction-entity database-entity difference]}]
+    (let [{:keys [filename line-number]} (:unify/annotations transaction-entity)]
+      (str "File: " filename " at line-number: " line-number "\n"
+           (apply str
+                  (text/->pretty-string (dissoc transaction-entity :unify/annotations :unify/identifying-avpair))
+                  (for [attr (keys difference)]
+                    (str "\nTransaction file specified " attr " with value "
+                         (get transaction-entity attr)
+                         "\nDatabase contains " attr " with value "
+                         (get database-entity attr)))))))
 
-(defn crosscheck-reference
-  [{:keys [target-dir] :as ctx}]
-  (let [tx-data-dir (str target-dir "/" "tx-data")]
-    (when-not (util.io/exists? tx-data-dir)
-      (log/error "What happened to error handling here?")
-      (exit 1 "Directory with transaction data must already exist when crosscheck references."))
-    (let [crosscheck-result (import/crosscheck-references ctx)]
-      (if (empty? crosscheck-result)
-        (println "Reference data crosscheck complete. No reference data collisions detected.")
-        (do
-          (println "Crosscheck Complete. Collisions detected:")
-          (println "-----------------------------------------")
-          (doseq [conflict crosscheck-result]
-            (println (format-crosscheck-conflict conflict))
-            (println "---------------------")))))))
+#_(defn crosscheck-reference
+    [{:keys [target-dir] :as ctx}]
+    (let [tx-data-dir (str target-dir "/" "tx-data")]
+      (when-not (util.io/exists? tx-data-dir)
+        (log/error "What happened to error handling here?")
+        (exit 1 "Directory with transaction data must already exist when crosscheck references."))
+      (let [crosscheck-result (import/crosscheck-references ctx)]
+        (if (empty? crosscheck-result)
+          (println "Reference data crosscheck complete. No reference data collisions detected.")
+          (do
+            (println "Crosscheck Complete. Collisions detected:")
+            (println "-----------------------------------------")
+            (doseq [conflict crosscheck-result]
+              (println (format-crosscheck-conflict conflict))
+              (println "---------------------")))))))
 
 
 (def tasks
   {
    "request-db" request-db
    "prepare" prepare
-   "diff" diff
+;;   "diff" diff
    "transact" transact
-   "validate" validate
-   "crosscheck-reference" crosscheck-reference
+;;   "validate" validate
+;;   "crosscheck-reference" crosscheck-reference
    "list-dbs" list-dbs
    "delete-db" delete-db})
 
 (def allowed-tasks (set (keys tasks)))
 
-
-;; Main parse -------------------------------------------------------
-;;
 
 (defn validate-args
   "Validate command line arguments. Either return a map indicating the program
@@ -310,7 +310,6 @@
                 skip-annotations
                 overwrite
                 resume
-                validations
                 update]} options
         task (first arguments)]
     (cond-> {:task task
@@ -318,7 +317,6 @@
       database (assoc :datomic-uri (database-name->datomic-uri task database))
       import-config (assoc :import-cfg-file import-config)
       working-directory (assoc :target-dir working-directory)
-      validations (assoc :validations-file validations)
       options (assoc :options options))))
 
 
@@ -328,16 +326,18 @@
 
 
 (defn -main [& args]
+  (println unify-ascii)
   (try
-    (println "unify version:" (release/version))
-    (let [argmap (validate-args args)
-          {:keys [exit-message ok?]} argmap]
+    (println "version:" (release/version))
+    (let [arg-map (validate-args args)
+          {:keys [exit-message ok?]} arg-map]
       (if exit-message
         (exit (if ok? 0 1) exit-message)
-        (let [{:keys [task options] :as parsed} (parse-task-args argmap)
+        (let [{:keys [task options] :as parsed} (parse-task-args arg-map)
               start (Date.)
               task-args (merge parsed options)
-              task-results ((get tasks task) task-args)]
+              task-fn (get tasks task)
+              task-results (task-fn task-args)]
           (println "Took" (elapsed start (Date.)) "seconds")
           (if (:errors task-results)
             (exit 1 (str "Task: " task " failed "))
