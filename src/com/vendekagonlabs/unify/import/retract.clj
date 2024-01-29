e129f7d24a800be24629a1eac5843276475dcf82
Ben Kamphaus <ben.kamphaus@gmail.com>
2024-01-28 18:44:17 -0700

    continue prototyping import data gathering after metadata update

--- src/com/vendekagonlabs/unify/import/retract.clj
+++ src/com/vendekagonlabs/unify/import/retract.clj
@@ -8,27 +8,53 @@
 
 (defn kinds->ids [])
 
+(defn dataset->imports
+  [db dataset-name]
+  (map first
+       (d/q '[:find ?import-name
+              :in $ ?dataset-name
+              :where
+              [?d :dataset/name ?dataset-name]
+              [?ui :unify.import/name ?import-name]
+              [?ui :unify.import/dataset ?d]]
+            db dataset-name)))
+
+(defn import->txes
+  [db import-name]
+  (map first
+       (d/q '[:find ?tx
+              :in $ ?import-name
+              :where
+              [?i :unify.import/name ?import-name]
+              [?tx :unify.import.tx/import ?i]]
+            db import-name)))
+
 (defn dataset->entities
   [db dataset-name])
 
 (comment
   :db-setup
-  (def db-name "unify-example")
+  (def db-name "retract-test")
   (def db-info (db/fetch-info db-name))
-  (def db (db/latest-db db-info)))
+  (def db (db/latest-db db-info))
+  (def conn (db/get-connection db-info)))
 
 (comment
   :queries
   (require '[datomic.api :as d])
+  (def imports
+    (dataset->imports db "matrix-test"))
+
+  (def txes (import->txes db (first imports)))
+  (def lower-tx (reduce min txes))
+  (def upper-tx (reduce max txes))
+  (def tx-seq (d/tx-range (d/log conn) lower-tx (inc upper-tx)))
+  (:data (first tx-seq))
+
   (d/q '[:find ?dname
          :in $
          :where
          [?d :dataset/name ?dname]]
-       db)
-  (d/q '[:find ?import-name (pull ?i [*])
-         :in $
-         :where
-         [?i :import/name ?import-name]]
        db))
 
 (comment
