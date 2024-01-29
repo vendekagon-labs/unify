e73d7233ef8fea4380d90d89be42c0d9ce3dd3cf
Ben Kamphaus <ben.kamphaus@gmail.com>
2024-01-28 20:02:20 -0700

    wip: sketching remainder of retract dataset fn

--- src/com/vendekagonlabs/unify/import/retract.clj
+++ src/com/vendekagonlabs/unify/import/retract.clj
@@ -2,11 +2,18 @@
   (:require [com.vendekagonlabs.unify.db.schema :as schema]
             [com.vendekagonlabs.unify.db :as db]))
 
-(defn schema [db]
-  (schema/get-metamodel-and-schema db))
 
-
-(defn kinds->ids [])
+(defn schema->kind-ids
+  [schema]
+  (->> schema
+       (first)  ; index by kinds is first pos of schema
+       (:index/kinds)
+       (remove (fn [[_ ent]]
+                 (:unify.kind/ref-data ent)))
+       (map (fn [[_ ent]]
+              (or (get-in ent [:unify.kind/need-uid :db/ident])
+                  (get-in ent [:unify.kind/global-id :db/ident]))))
+       (remove :dataset/name)))
 
 (defn dataset->imports
   [db dataset-name]
@@ -29,8 +36,18 @@
               [?tx :unify.import.tx/import ?i]]
             db import-name)))
 
-(defn dataset->entities
-  [db dataset-name])
+(defn dataset->entity-ids
+  [db dataset-name]
+  ;; TODO: `first` logic will have to change after diff merge, maybe just
+  ;;       throw and rule out retract in that case?
+  (let [import (first (dataset->imports db dataset-name))
+        schema (schema/get-metamodel-and-schema db)
+        txes (import->txes db import)
+        start-tx (reduce min txes)
+        stop-tx (inc (reduce max txes))]))
+    ;; and then filter tx-range to start and tx positions
+    ;; then scan over and find all assertions of unique kind ids
+    ;; then batch transactions of N size of retractEntity tx fn call
 
 (comment
   :db-setup
@@ -49,21 +66,9 @@
   (def lower-tx (reduce min txes))
   (def upper-tx (reduce max txes))
   (def tx-seq (d/tx-range (d/log conn) lower-tx (inc upper-tx)))
-  (:data (first tx-seq))
-
-  (d/q '[:find ?dname
-         :in $
-         :where
-         [?d :dataset/name ?dname]]
-       db))
+  (:data (first tx-seq)))
 
 (comment
   :schema-info
   (def schema (schema/get-metamodel-and-schema db))
-  (def index (first schema))
-  (keys index)
-  (def kinds-best-id
-    (map (fn [[_ ent]]
-           (or (get-in ent [:unify.kind/need-uid :db/ident])
-               (get-in ent [:unify.kind/global-id :db/ident])))
-         (:index/kinds index))))
+  (schema->kind-ids schema))
